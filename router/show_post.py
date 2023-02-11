b972691c685070e74218ba55a265e57ac0f93405
YINGMEI <j.yeongmee@gmail.com>
2023-02-12 02:56:48 +0900

    게시물 보여주기 기능 구현 수정, 게시물 수정하기 기능 구현 완료

--- router/show_post.py
+++ router/show_post.py
@@ -21,6 +21,7 @@ def home():
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
         user_info = db.user.find_one({"id": payload['id']})
+        print(user_info['id'])
         return render_template('show_post/index.html', user_id=user_info["id"])
     except jwt.ExpiredSignatureError:
         return render_template('index.html')
@@ -28,14 +29,30 @@ def home():
         return render_template('index.html')
 
 
-@show_post.route('/login', methods=['GET'])
-def login():
-    payload = {
-        'id': 'test1',
-        'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24)
-    }
-    token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-    return jsonify({'token': token})
+# @show_post.route('/login', methods=['GET'])
+# def login():
+#     payload = {
+#         'id': 'test1',
+#         'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24)
+#     }
+#     token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+#     return jsonify({'token': token})
+
+@show_post.route('/update', methods=['POST'])
+def update():
+    user_id = request.form['id']
+    dog_name = request.form['dog_name']
+    comment = request.form['comment']
+
+    print(user_id, dog_name, comment)
+    comments = db.user.find_one({'id': user_id}, {'_id': False})['comments']
+    for i in range(len(comments)):
+        if comments[i]['dog_name'] == dog_name:
+            comments[i]['comment'] = comment
+
+    db.user.update_one({'id': user_id}, {'$set': {'comments': comments}})
+    return jsonify({'msg': '수정완료!'})
+
 
 @show_post.route('/show_post', methods=["GET"])
 def show():
@@ -47,6 +64,7 @@ def show():
     users = list(db.user.find({}, {'_id': False}))
     comments = db.user.find_one({'id': user_id}, {'_id': False})['comments']
 
+    print(type(dog['dog_name']))
     # print(dog['dog_name'], dog['dog_image'], dog['dog_intro'])
 
     return jsonify({'msg': '연결 완료!', 'dog': dog, 'comments':comments, 'users':users})
