6075aaf8a6e4d5eccb873d88bbdb9182894a5f7e
YINGMEI <j.yeongmee@gmail.com>
2023-02-12 04:53:41 +0900

    최종으로 기능 수정 완료 보여주기, 수정하기, 삭제하기

--- router/show_post.py
+++ router/show_post.py
@@ -3,9 +3,7 @@ from pymongo import MongoClient
 import certifi
 
 import jwt
-import datetime
 
-import hashlib
 
 ca = certifi.where()
 client = MongoClient('mongodb+srv://project:ydmd5@cluster0.n7giicj.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
@@ -17,28 +15,21 @@ SECRET_KEY = 'YDMD'
 
 @show_post.route('/')
 def home():
-
-    # return render_template('show_post/index.html')
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
         user_info = db.user.find_one({"id": payload['id']})
-        print(user_info['id'])
-        return render_template('show_post/index.html', user_id=user_info["id"])
+        # 토큰이 유효한데 db에 유저가 삭제되면 find_one이 null이 되면서 TypeError 발생
+        if user_info is None:
+            return render_template('/index.html', result="logout")
+        else:
+            return render_template('show_post/index.html', result="login",
+                                   id=user_info['id'])
     except jwt.ExpiredSignatureError:
-        return render_template('index.html')
+        return render_template('/index.html', result="logout")
     except jwt.exceptions.DecodeError:
-        return render_template('index.html')
+        return render_template('/index.html', result="logout")
 
-
-# @show_post.route('/login', methods=['GET'])
-# def login():
-#     payload = {
-#         'id': 'test1',
-#         'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24)
-#     }
-#     token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-#     return jsonify({'token': token})
 
 @show_post.route('/update', methods=['POST'])
 def update():
@@ -56,6 +47,22 @@ def update():
     return jsonify({'msg': '수정완료!'})
 
 
+@show_post.route('/delete', methods=['DELETE'])
+def delete():
+    user_id = request.form['id']
+    dog_name = request.form['dog_name']
+    comment = request.form['comment']
+
+    print(user_id, dog_name, comment)
+    comments = db.user.find_one({'id': user_id}, {'_id': False})['comments']
+    for i in range(len(comments)):
+        if comments[i]['dog_name'] == dog_name and comments[i]['comment'] == comment:
+            del comments[i]
+
+    db.user.update_one({'id': user_id}, {'$set': {'comments': comments}})
+    return jsonify({'msg': '삭제완료!'})
+
+
 @show_post.route('/show_post', methods=["GET"])
 def show():
     user_id = request.args['user_id']
@@ -66,8 +73,5 @@ def show():
     users = list(db.user.find({}, {'_id': False}))
     comments = db.user.find_one({'id': user_id}, {'_id': False})['comments']
 
-    print(type(dog['dog_name']))
-    # print(dog['dog_name'], dog['dog_image'], dog['dog_intro'])
-
-    return jsonify({'msg': '연결 완료!', 'dog': dog, 'comments':comments, 'users':users})
+    return jsonify({'dog': dog, 'comments':comments, 'users':users})
 
